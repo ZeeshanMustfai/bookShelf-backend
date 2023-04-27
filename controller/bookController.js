b5aeb06a06a4688c765ed9d2e73213bb42d3d569
Zeeshan Mustfai <zeeshanmustfai22@gmail.com>
2023-04-27 18:08:19 +0500

    add search route

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,64 +1,87 @@
-const BookModal = require("../models/Books")
+const BookModal = require('../models/Books')
 
 const AddNewBook = async (req, res) => {
-  const Book =  new BookModal(req.body)
-  try{
-    const response = await Book.save()
-    res.status(200).json({message: 'New Book added successfully', response})
-  }catch(e){
-    res.status(400).json({message: 'Some thing went wrong', error: e?.message})
-  }
-  
+	const Book = new BookModal(req.body)
+	try {
+		const response = await Book.save()
+		res.status(200).json({ message: 'New Book added successfully', response })
+	} catch (e) {
+		res
+			.status(400)
+			.json({ message: 'Some thing went wrong', error: e?.message })
+	}
 }
 
 const GetAllBooks = async (req, res) => {
-  try{
-     const books = await BookModal.find()
-      res.status(200).json({message: 'All book of this user', books})
-  }catch(e){
-    res.status(400).json({message: 'Some thing went wrong'})
-  }
- 
+	try {
+		const books = await BookModal.find()
+		res.status(200).json({ message: 'All book of this user', books })
+	} catch (e) {
+		res.status(400).json({ message: 'Some thing went wrong' })
+	}
 }
 
 const UpdateOneBook = async (req, res) => {
-  try{
-    const book = await BookModal.findById(req.body.id);
-  if (book) {
-    console.log('book', book)
-    res.send('book found')
-    // try {
-    //   const updatedBook = await BookModal.findByIdAndUpdate(
-    //     req.body.id,
-    //     { $set: req.body },
-    //     { new: true }
-    //   );
-    //   res
-    //     .status(200)
-    //     .json({ message: "Book updated Successfully", result: updatedBook });
-    // } catch (err) {
-    //   res.status(400).json(err);
-    // }
-  } else {
-    res.status(201).json({ message: "Book not found for update" });
-  }
-  }catch(e){
-    res.status(400).json({ message: "Something went wrong" });
-  }
+	try {
+		const book = await BookModal.findById(req.body.id)
+		if (book) {
+			try {
+				const updatedBook = await BookModal.findByIdAndUpdate(
+					req.body.id,
+					{ $set: { status: req.body.status } },
+					{ new: true }
+				)
+				res
+					.status(200)
+					.json({ message: 'Book updated Successfully', result: updatedBook })
+			} catch (err) {
+				res.status(400).json(err)
+			}
+		} else {
+			res.status(201).json({ message: 'Book not found for update' })
+		}
+	} catch (e) {
+		res.status(400).json({ message: 'Something went wrong' })
+	}
+}
+
+const SearchBooks = async (req, res) => {
+	try {
+		let result = await BookModal.find({
+			$or: [
+				{
+					title: { $regex: req.params.key },
+				},
+				{
+					authorName: { $regex: req.params.key },
+				},
+			],
+		})
+		if (result) {
+			res
+				.status(200)
+				.json({ message: 'Books found successfully', books: result })
+		} else {
+			res.status(400).json({ message: 'Books not found' })
+		}
+	} catch (err) {
+		res.status(400).json({ message: 'Books not found' })
+	}
 }
 
 const DeleteOneBook = async (req, res) => {
-  try{
-    const deletedBook = await BookModal.deleteOne({owner: req.params.id})
-    res.json({message: 'Book is deleted successfully', deletedBook})
-  }catch(e){
-    res.json({message: 'Some thing went wrong'})
-  }
+	try {
+		const deletedBook = await BookModal.deleteOne({ owner: req.params.id })
+		res.json({ message: 'Book is deleted successfully', deletedBook })
+	} catch (e) {
+		res.json({ message: 'Some thing went wrong' })
+	}
 }
 
 module.exports = {
-  AddNewBook,
-  GetAllBooks,
-  DeleteOneBook,
-  UpdateOneBook
+	AddNewBook,
+	GetAllBooks,
+	DeleteOneBook,
+	UpdateOneBook,
+	SearchBooks,
 }
--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,17 @@
-const {AddNewBook, GetAllBooks, DeleteOneBook, UpdateOneBook} = require("../controller/bookController");
-const { requireAuth } = require("../middleware/authMiddleware");
-const Router = require("express").Router();
+const {
+	AddNewBook,
+	GetAllBooks,
+	DeleteOneBook,
+	UpdateOneBook,
+	SearchBooks,
+} = require('../controller/bookController')
+const { requireAuth } = require('../middleware/authMiddleware')
+const Router = require('express').Router()
 
+Router.get('/', requireAuth, GetAllBooks)
+Router.get('/search/:key', requireAuth, SearchBooks)
 Router.post('/create', requireAuth, AddNewBook)
-Router.get('/', GetAllBooks)
 Router.put('/update', requireAuth, UpdateOneBook)
-Router.delete("/:id",requireAuth, DeleteOneBook)
-
+Router.delete('/:id', requireAuth, DeleteOneBook)
 
 module.exports = Router
